e938b7cdbf2b245f3c35873f49d171d5738220a8
Aaden hampton <ah126007@student.wpsok.org>
2025-09-23 13:24:44 -0500

    Playground.py

---
+++ Playground.py
@@ -0,0 +1,17 @@
+#Name: Aaden Hampton
+#Class: 6th Hour
+#Assignment:playground
+import random
+from random import choice
+
+print("Hello World")
+num_list=[232, 9, 6, 7,44, 3, 79, 55,675]
+print(num_list)
+num_list.sort(reverse=True)
+print(num_list)
+empty_list=[]
+print(empty_list)
+name_list2=["Aaden", "Devon", "Dyson", "Carter", "kevin", "Carlos", "Aiden", "Brennlyn", "Benjamin", "Kiah", Annie]
+print(name_list2)
+name_list2.remove(random.choice(name_list2))
+print(name_list2)
