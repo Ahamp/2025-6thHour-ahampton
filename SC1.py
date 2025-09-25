07854553f2e6bfae2d06e8394823edf903f2d597
Aaden hampton <ah126007@student.wpsok.org>
2025-09-25 14:08:26 -0500

    SC1

--- SC1.py
+++ SC1.py
@@ -11,3 +11,55 @@
 #to confirm they went through.
 
 #It is up to you to decide what properties are important and the theme of the game.
+Rustcardict={
+    "enemy1":{
+
+        "Name": "willjum",
+        "height": "6'3",
+        "health": "150",
+        "damage": 75
+
+    },
+    "enemy2": {
+
+        "Name": "Jorque",
+        "height": "5'4",
+        "health": "300",
+        "damage": 67
+
+    },
+    "enemy3": {
+
+        "Name": "Aaden",
+        "height": "6'6",
+        "health": "175",
+        "damage": 140
+
+    },
+    "enemy4": {
+
+        "Name": "Blooprint",
+        "height": "6'8",
+        "health": "150",
+        "damage": 85
+
+
+    },
+    "enemy5": {
+
+        "Name": "Caleb",
+        "height": "5'10",
+        "health": "200",
+        "damage": 100
+    }
+}
+Rustcardict["enemy1"]["damage"]=int(input())
+print(Rustcardict["enemy1"])
+Rustcardict["enemy2"]["damage"]=int(input())
+print(Rustcardict["enemy2"])
+Rustcardict["enemy3"]["damage"]=int(input())
+print(Rustcardict["enemy3"])
+Rustcardict["enemy4"]["damage"]=int(input())
+print(Rustcardict["enemy4"])
+Rustcardict["enemy5"]["damage"]=int(input())
+print(Rustcardict["enemy5"])
